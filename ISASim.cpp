e1b0bcf9653e48b0911e527f89067346fc7f2a88
mohanjan <jwbartling.johan@gmail.com>
2022-11-28 10:42:40 +0100

    i think JAL and JALR are sus

--- ISASim.cpp
+++ ISASim.cpp
@@ -25,8 +25,11 @@
 #include <string>
 #include <cstdint>
 
-void read_bin(std::string fileloc, uint32_t mem[]);
-void write_bin(uint32_t mem[]);
+void read_bin(std::string fileloc, uint8_t mem[]);
+void write_bin(uint8_t mem[]);
+uint32_t get_inst(uint8_t mem[], int32_t pc);
+uint32_t get_mem(uint8_t mem[], int32_t address, int32_t size);
+void set_mem(uint8_t mem[], int32_t address, int32_t size, uint32_t regval);
 #define NO_ERR 0;
 
 using namespace std;
@@ -36,34 +39,26 @@ int main(void) {
     int32_t pc(0);
     uint32_t i;
     array<uint32_t, 32> reg;
-    // Here the first program hard coded as an array
-    //array<uint32_t, 10000> memo;
-    uint32_t memo[10000];
-    array<uint32_t, 7> inst = {
-        0x00a00513,
-        0x01400593,
-        0x00b50633,
-        0x00200693,
-        0x00269713,
-        0x00175793,
-        0x40c58533
-    };
+    uint8_t memo[800000];
 
     for(i = 0; i<= 31;i++){
         reg[i] = 0;
     }
     reg[2] = sizeof(memo)/sizeof(uint32_t);
-
+    reg[3] = sizeof(memo)/sizeof(uint32_t)/2;
     i = 0;
-
+    
     while(i < sizeof(memo)/sizeof(uint32_t)){
         memo[i] = 0;
         i++;
     }
 
-    std::string fileloc = "task2/branchcnt.bin";
-    cin>> fileloc;
-    
+    std::string fileloc = "task4/t";
+    //std::string fileloc = "task4/t11.bin";
+    int test;
+    cin>> test;
+    fileloc += to_string(test) + ".bin";
+   
     std::cout << fileloc << endl;
 
     read_bin(fileloc, memo);
@@ -75,44 +70,55 @@ int main(void) {
     while(1) {
         reg[0] = 0;
     
-        uint32_t instr = memo[pc >> 2];
+        //uint32_t instr = memo[pc >> 2];
+        uint32_t instr = memo[pc] + (memo[pc+1] << 8) + (memo[pc+2] << 16)+ (memo[pc+3] <<24);
         uint32_t opcode = instr & 0x7f;
         uint32_t rd = (instr >> 7) & 0x01f;
         uint32_t rs1 = (instr >> 15) & 0x01f;
         uint32_t rs2 = (instr >> 20) & 0x01f;
+        uint32_t funct3 = ((instr >> 12) & 0b111);
+        uint32_t funct7 = (instr >> 25);
+
         uint32_t imm = (instr >> 20);
         if(imm & 0x800 == 0x800){
             imm = imm | 0x800000000;
         }
-        uint32_t imm5 = (instr >> 20) & 0b11111;
-        uint32_t immU = (instr >> 12) << 12;
-        uint32_t immS = (instr >> 25) << 5 | ((instr >> 7) & 0b11111); //todo: add signage
-        //                 --4:1--                           --12|10:5                                         --11--
+
+        int32_t immS = ((instr >> 25) & 0x7F) << 5 | ((instr >> 7) & 0b11111);
+        if ((immS & 0x1000) == 0x1000){
+            immS = immS | 0xFFFFF000;
+        }
+
         int32_t immB = (((instr >> 8) & 0b1111) | (((instr >> 25) & 0b111111) << 4) | ((instr >> 31) << 11) | (((instr & 0x80) >> 7) << 10) ) << 1;
         if ((immB & 0x1000) == 0x1000){
-            immB = (immB)| 0xFFFFF000 ;
+            immB = immB | 0xFFFFF000;
         }
-        uint32_t funct3 = ((instr >> 12) & 0b111);
-        uint32_t funct7 = (instr >> 25);
 
-
+        //                  --10:1--                    --11--                      --19:12--                          --20--
+        int32_t immJ = (((instr >> 21) & 0x3FF) << 1) | (((instr >> 20) & 0b1) << 10) | (((instr >> 12) & 0xF) << 11) | (instr & 0x80000000)>>12;
+        if ((immJ & 0x1000) == 0x1000){
+            immJ = immJ | 0xFFFFF000;
+        }
+        
         switch (opcode) {
 
             case 0x37: //LUI
-                reg[rd] = immU;
+                reg[rd] = (instr >> 12) << 12;
             break;
             case 0x17: //AUIPC
-                reg[rd] = immU + pc;
+                reg[rd] = ((instr >> 12) << 12) + pc;
             break;
             
             //----------Jump & branch instructions----------
             case 0x6F: //JAL
                 reg[rd] = pc+4;
-                pc = imm;
-            break;
+                pc += immJ - 4;
+                break;
             case 0x67: //JALR
-                
-            break;
+                reg[rd] = pc+4;
+                pc = (imm + reg[rs1]) & 0xFFFFFFFE - 4;
+                break;
+
             case 0x63: //BEQ/BNE/BLTU/BGEU (funct 3 value)
                 switch (funct3)
                 {
@@ -161,40 +167,41 @@ int main(void) {
             //----------Load instructions----------
             case 0x3: // LB/LH/LW/LBU/LHU (funct 3 value)
 
-            switch (funct3)
+            switch (funct3) //well since my memory is segmented into words and not bytes, there is a problem with LH and LW types
             {
                 case 0b000: //LB
-                    if (int32_t(( memo[(imm + reg[rs1])>>2] & 0xFF ) < 0))
+                    if (( get_mem(memo, (imm + reg[rs1]), 1) & 0x80 ) == 0x80)
                     {
-                        reg[rd] = (memo[(imm + reg[rs1])>>2] & 0xFF); //todo: sign extend
+                        reg[rd] = (get_mem(memo, (imm + reg[rs1]), 1) &0xFF) | 0xFFFFFF00;
                     }
                     else{
-                        reg[rd] = memo[(imm + reg[rs1])>>2] & 0xFF;
+                        reg[rd] = get_mem(memo, (imm + reg[rs1]), 1) & 0xFF;
                     }
                         
                     break;
                 
                 case 0b001: //LH
-                    if (int32_t(( memo[(imm + reg[rs1]) >> 2] & 0xFFFF ) < 0))
+                    //new attempt
+                    if ((get_mem(memo, imm + reg[rs1], 2) & 0x8000 ) == 0x8000)
                     {
-                        reg[rd] = (memo[(imm + reg[rs1]) >> 2] & 0xFFFF ); //todo: sign extend
+                        reg[rd] = ((get_mem(memo, (imm + reg[rs1]), 2) & 0xFFFF) | 0xFFFF0000 );
                     }
                     else{
-                        reg[rd] = memo[(imm + reg[rs1]) >> 2] & 0xFFFF;
+                        reg[rd] = get_mem(memo, (imm + reg[rs1]), 2) & 0xFFFF;
                     }
                         
                     break;
 
-                case 0b010: //LW
-                    reg[rd] = memo[(imm + reg[rs1]) >> 2];
+                case 0b010: //LW 
+                    reg[rd] = get_mem(memo, (imm + reg[rs1]), 4);
                     break;
                 
                 case 0b100: //LBU
-                    reg[rd] = memo[(imm + reg[rs1]) >> 2] & 0xFF;
+                    reg[rd] = get_mem(memo, (imm + reg[rs1]), 1) & 0xFF;
                     break;
                 
                 case 0b101: //LHU
-                    reg[rd] = memo[(imm + reg[rs1]) >> 2] & 0xFFFF;
+                    reg[rd] = get_mem(memo, (imm + reg[rs1]), 2) & 0xFFFF;
                     break;
             
             }
@@ -205,15 +212,18 @@ int main(void) {
             switch (funct3)
             {
                 case 0b000: 
-                    memo[(rs1 + immS) >> 2] = rs2 & 0xff;
+                    set_mem(memo, (reg[rs1] + immS), 1, reg[rs2]);
+                    //memo[(reg[rs1] + immS) >> 2] = reg[rs2] & 0xff;
                     break;
 
                 case 0b001:
-                    memo[(rs1 + immS) >> 2] = rs2 & 0xffff;
+                    set_mem(memo, (reg[rs1] + immS), 2, reg[rs2]);
+                    //memo[(reg[rs1] + immS) >> 2] = reg[rs2] & 0xffff;
                     break;
 
                 case 0b010:
-                    memo[(rs1 + immS) >> 2] = rs2;
+                    set_mem(memo, (reg[rs1] + immS), 4, reg[rs2]);
+                    //memo[(reg[rs1] + immS) >> 2] = reg[rs2];
                     break;
                 }    
                 break;
@@ -379,8 +389,8 @@ int main(void) {
                 break;
         }
         
-        pc += 4; // One instruction is four bytes
-        if (memo[pc>>2] == 0 || memo[pc>>2] == 0x73)
+        pc += 4;
+        if (get_inst(memo,pc) == 0 || get_inst(memo,pc) == 0x73)
             break;
 
         //if ((pc >> 2) >= memo.size()) {
@@ -401,7 +411,44 @@ int main(void) {
     return NO_ERR;
 }
 
-void read_bin(std::string fileloc, uint32_t mem[]){
+uint32_t get_inst(uint8_t mem[], int32_t pc){
+    return mem[pc] +(mem[pc+1]<<8)+(mem[pc+2]<<16)+(mem[pc+3]<<16);
+}
+
+uint32_t get_mem(uint8_t mem[], int32_t address, int32_t size){
+    if(size == 1){
+        return mem[address];
+    }
+    else if (size == 2)
+    {
+        return mem[address] + (mem[address+1] << 8);
+    }
+    else if (size == 4)
+    {
+        return mem[address] + (mem[address+1] << 8) + (mem[address+2] << 16) + (mem[address+3] << 24);
+    }
+}
+
+void set_mem(uint8_t mem[], int32_t address, int32_t size, uint32_t regval){
+    if(size == 1){
+        mem[address] = regval & 0xFF;
+    }
+    else if (size == 2)
+    {
+        mem[address] = regval & 0xFF;
+        mem[address+1] = (regval & 0xFF00) >>8;
+        
+    }
+    else if (size == 4)
+    {
+        mem[address] = regval & 0xFF;
+        mem[address+1] = (regval & 0xFF00) >> 8;
+        mem[address+2] = (regval & 0xFF0000) >> 16;
+        mem[address+3] = (regval & 0xFF000000) >> 24;
+    }
+}
+
+void read_bin(std::string fileloc, uint8_t mem[]){
     std::streampos size;
     int byte_length;
     char * memblock;
@@ -416,14 +463,24 @@ void read_bin(std::string fileloc, uint32_t mem[]){
         file.seekg (0, std::ios::beg);
         memblock = new char [size];
         file.read (memblock, size);
+        
+        //new byte adressed version
+        for(int i = 0;i < byte_length;i++){
+            
+                file.seekg(i, std::ios::beg);
+                file.read(memblock,1);
+                mem[i] = uint32_t(memblock[i] & 0xff);
+            
+        }
 
+        /* old version
         for(int i = 0;i < byte_length/4;i++){
             for(int j = 0; j < 4; j++){
                 file.seekg(j + 4*i, std::ios::beg);
                 file.read(memblock,1);
                 mem[i] |= (uint32_t(memblock[i*4+j] & 0xff) << j*8);
             }
-        }
+        }*/
         file.close();
 
     }
@@ -431,12 +488,13 @@ void read_bin(std::string fileloc, uint32_t mem[]){
 
 }
 
-
-void write_bin(uint32_t mem[]){
+void write_bin(uint8_t mem[]){
     int i = 0;
-    while(!(mem[i]==0) && i<100){
-        std::cout<< hex <<mem[i] <<"\n";
-        i++;
+    uint32_t pinst;
+    while(!(mem[i]==0 && mem[i+1]==0 && mem[i+2]==0 && mem[i+3]==0)){
+        pinst = mem[i] +(mem[i+1]<<8)+(mem[i+2]<<16)+(mem[i+3]<<16);
+        std::cout<< hex << pinst <<"\n";
+        i+=4;
     }
 
 }
@@ -444,23 +502,33 @@ void write_bin(uint32_t mem[]){
 
 
 
-
 /*
-void big_switch(uint32_t opcode){
+void process_instruction(uint32_t opcode, ){
 
     switch (opcode) {
 
             case 0x37: //LUI
-                
+                reg[rd] = (instr >> 12) << 12;
                 break;
+
             case 0x17: //AUIPC
-                
+                reg[rd] = ((instr >> 12) << 12) + pc;
                 break;
             
             //----------Jump & branch instructions----------
-            case 0b1100011:
-            reg[rd] = B_type();
-            break;
+            case 0x6F: //JAL
+                reg[rd] = pc+4;
+                pc = imm;
+                break;
+            case 0x67: //JALR
+
+                
+                break;
+            case 0x63: //BEQ/BNE/BLTU/BGEU (funct 3 value)
+                pc = B_type();
+            
+                break;
+            
 
             //----------Load instructions----------
             case : // LB/LH/LW/LBU/LHU (funct 3 value)
@@ -497,8 +565,49 @@ void S_type(){
     
 }
 
-void B_type(){
-    
+uint32_t B_type(uint32_t funct3, uint32_t reg[],uint32_t rs1, uint32_t rs2 , uint32_t pc, int32_t immB){
+    switch (funct3)
+        {
+        case 0b000: //BEQ
+            if(reg[rs1] == reg[rs2]){
+                    pc += immB - 4;                        
+            }
+            break;
+
+        case 0b001: //BNE
+            if(reg[rs1] != reg[rs2]){
+                    pc += immB - 4;
+            }
+            break;
+
+        case 0b100: //BLT
+            if(int32_t(reg[rs1]) < int32_t(reg[rs2])){
+                pc += immB - 4;
+            }
+            
+            break;
+
+        case 0b101: //BGE
+            if(int32_t(reg[rs1]) >= int32_t(reg[rs2])){
+                pc += immB - 4;
+            }
+            
+            break;
+
+        case 0b110: //BLTU
+            if(reg[rs1] < reg[rs2]){
+                pc += immB - 4;
+            }
+            
+            break;  
+
+        case 0b111: //BGEU
+            if(reg[rs1] > reg[rs2]){
+                pc += immB - 4;
+            }
+            
+            break;
+        }
 }
 
 void U_type(){
